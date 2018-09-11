ce446d56d59f9b21aa84bca89c523a533dca68b5
Sebastiaann <sebastiaanbarneveld@hotmail.com>
2018-09-11 17:10:07 +0200

    Update group42.java
    begin

--- group42.java
+++ group42.java
@@ -15,6 +15,12 @@ public class group42 implements ContestSubmission
 		rnd_ = new Random();
 	}
 	
+	public static void Main(){
+		
+		        System.out.println("test1");
+
+	}
+	
 	public void setSeed(long seed)
 	{
 		// Set seed of algortihms random process
@@ -47,12 +53,28 @@ public class group42 implements ContestSubmission
 	public void run()
 	{
 		// Run your algorithm here
-        
+        System.out.println("test3");
         int evals = 0;
         // init population
+		int popsize = 25;
+		int ran;
+		double children[][] = new double[popsize][10];
+		for(int i = 0; i <popsize; i++){
+			for(int j = 0; j <10; j++){
+				ran = rnd_.nextInt(2);
+				children[i][j] = ran;
+			}
+		}
+		
+		
         // calculate fitness
+		System.out.println(findFitness(children[3]));
+		
+		
+		
         while(evals<evaluations_limit_){
             // Select parents
+			
             // Apply crossover / mutation operators
             double child[] = {0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0};
             // Check fitness of unknown fuction
@@ -62,4 +84,12 @@ public class group42 implements ContestSubmission
         }
 
 	}
+	
+	public int findFitness(double child[]){
+		int findFitness=0;
+		for(int i = 0; i < 10; i++){findFitness+=child[i];}
+		return findFitness;
+	}
+	
+
 }
